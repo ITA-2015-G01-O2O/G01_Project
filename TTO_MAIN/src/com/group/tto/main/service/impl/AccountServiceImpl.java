66f27e3e3dbb2814ce9a81da5b429b05a1b3b795
chenac <ace.chen@oocl.com>
2015-08-24 15:24:45 +0800

    update the AccountServiceImpl.java

--- TTO_MAIN/src/com/group/tto/main/service/impl/AccountServiceImpl.java
+++ TTO_MAIN/src/com/group/tto/main/service/impl/AccountServiceImpl.java
@@ -1,11 +1,13 @@
 package com.group.tto.main.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.group.tto.cmn.model.Account;
 import com.group.tto.main.dao.AccountDao;
 import com.group.tto.main.service.AccountService;
 
+@Service
 public class AccountServiceImpl implements AccountService {
 
 	@Autowired
