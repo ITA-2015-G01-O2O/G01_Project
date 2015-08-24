025475d982b0a67d8afe4544e37c52b55bf181dc
chenac <ace.chen@oocl.com>
2015-08-24 17:45:40 +0800

    add the package to the projecrt

--- TTO_MAIN/src/com/group/tto/main/vendor/dao/impl/PoductDaoImpl.java
+++ TTO_MAIN/src/com/group/tto/main/vendor/dao/impl/PoductDaoImpl.java
@@ -1,5 +1,46 @@
 package com.group.tto.main.vendor.dao.impl;
 
-public class PoductDaoImpl {
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.stereotype.Repository;
+
+import com.group.tto.cmn.model.Product;
+import com.group.tto.main.dao.BaseDao;
+import com.group.tto.main.vendor.dao.PoductDao;
+
+@Repository
+public class PoductDaoImpl extends BaseDao<Product> implements PoductDao {
+
+	@Override
+	public List<Product> findAllProducts() {
+		CriteriaBuilder builder = this.getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Product> query = builder.createQuery(Product.class);
+	  //	Root<Product> product = query.from(Product.class);
+	     List <Product>products=this.getEntityManager().createQuery(query).getResultList();
+		return products;
+	}
+
+	@Override
+	public List<Product> findProductsByLabel(String label) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateProduct(Product product) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteProduct(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
