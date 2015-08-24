d6adb05c3df0513a5aafb543d140f5dd849055af
chenac <ace.chen@oocl.com>
2015-08-24 18:40:25 +0800

    update the dao

--- TTO_MAIN/src/com/group/tto/main/vendor/dao/PoductDao.java
+++ TTO_MAIN/src/com/group/tto/main/vendor/dao/PoductDao.java
@@ -1,11 +1,13 @@
 package com.group.tto.main.vendor.dao;
 
 import java.util.List;
+
 import com.group.tto.cmn.model.Product;
+import com.group.tto.cmn.model.ProductLabel;
 
 public interface PoductDao {
-	 public List<Product> findAllProducts();
-	 public List<Product> findProductsByLabel(String label);
+	 public List<Product> findAllProducts(String id);
+	 public List<Product> findProductsByLabel(String label,String id);
 	 public int updateProduct(Product product);
 	 public int deleteProduct(int id);
 }
--- TTO_MAIN/src/com/group/tto/main/vendor/dao/impl/PoductDaoImpl.java
+++ TTO_MAIN/src/com/group/tto/main/vendor/dao/impl/PoductDaoImpl.java
@@ -10,6 +10,7 @@ import javax.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 
 import com.group.tto.cmn.model.Product;
+import com.group.tto.cmn.model.ProductLabel;
 import com.group.tto.main.dao.BaseDao;
 import com.group.tto.main.vendor.dao.PoductDao;
 
@@ -17,18 +18,26 @@ import com.group.tto.main.vendor.dao.PoductDao;
 public class PoductDaoImpl extends BaseDao<Product> implements PoductDao {
 
 	@Override
-	public List<Product> findAllProducts() {
+	public List<Product> findAllProducts(String id) {
 		CriteriaBuilder builder = this.getEntityManager().getCriteriaBuilder();
 		CriteriaQuery<Product> query = builder.createQuery(Product.class);
-	  //	Root<Product> product = query.from(Product.class);
-	     List <Product>products=this.getEntityManager().createQuery(query).getResultList();
+		Root<Product> product=query.from(Product.class);
+		Predicate condition = builder.equal(product.get("STORE_ID"),id);
+	    List <Product>products=this.getEntityManager().createQuery(query.where(condition)).getResultList();
 		return products;
 	}
 
 	@Override
-	public List<Product> findProductsByLabel(String label) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Product> findProductsByLabel(String label,String id) {
+		
+		CriteriaBuilder builder = this.getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Product> query = builder.createQuery(Product.class);
+		Root<Product> product=query.from(Product.class);
+		
+		Predicate condition = builder.equal(product.get("storeId"),id);
+		builder.and(builder.equal(product.get("productLabel").get("productLabelName").as(String.class),label));
+	    List <Product>products=this.getEntityManager().createQuery(query.where(condition)).getResultList();
+		return products;
 	}
 
 	@Override
