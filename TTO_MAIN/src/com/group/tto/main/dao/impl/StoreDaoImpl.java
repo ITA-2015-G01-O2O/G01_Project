34464a07f55a7bfa17ae34ea462906b7c174d852
elvislinshengyu <450266403@qq.com>
2015-08-25 16:17:35 +0800

    store search controller
    store search service
    store search dao
    store search vo

--- TTO_MAIN/src/com/group/tto/main/dao/impl/StoreDaoImpl.java
+++ TTO_MAIN/src/com/group/tto/main/dao/impl/StoreDaoImpl.java
@@ -23,39 +23,44 @@ public class StoreDaoImpl extends BaseDao<Store> implements StoreDao {
   private static final String FIELD_STOREPLACE = "location";
 
   @Override
-	//根据商家id找到商家
+  // 根据商家id找到商家
   public Store getStoreById(int sid) {
     CriteriaBuilder builder = this.getEntityManager().getCriteriaBuilder();
     CriteriaQuery<Store> query = builder.createQuery(Store.class);
     Root<Store> store = query.from(Store.class);
-		
+
 
     Predicate condition = builder.equal(store.get(FIELD_STOREID), sid);
     Store store1 =
-		
-        this.getEntityManager().createQuery(query.where(condition)).getResultList().get(0);
+
+    this.getEntityManager().createQuery(query.where(condition)).getResultList().get(0);
     return store1;
   }
 
   @Override
   public List<Store> getStoreByCriteria(StoreSearchCriteria criteria) {
-    
+
     CriteriaBuilder builder = this.getEntityManager().getCriteriaBuilder();
     CriteriaQuery<Store> query = builder.createQuery(Store.class);
     Root<Store> store = query.from(Store.class);
-    
+
     Predicate condition = builder.equal(store.get(FIELD_STOREPLACE), criteria.getStoreLocation());
-    if(COMMON.ASC.equals(criteria.getOrderType())){
-      query=query.orderBy(builder.asc(store.get(criteria.getSortType())));
+
+    if (COMMON.ASC.equals(criteria.getOrderType())) {
+      query = query.orderBy(builder.asc(store.get(criteria.getSortType())));
     }
-    if(COMMON.DESC.equals(criteria.getOrderType())){
-      query=query.orderBy(builder.desc(store.get(criteria.getSortType())));
+    if (COMMON.DESC.equals(criteria.getOrderType())) {
+      query = query.orderBy(builder.desc(store.get(criteria.getSortType())));
     }
-    condition= builder.and(condition,builder.equal(store.get("typeConfig").get("configValue"), criteria.getStoreType()));
-    
+    if (criteria.getStoreType() != null || criteria.getSortType() != null) {
+      condition =
+          builder.and(condition,
+              builder.equal(store.get("typeConfig").get("configValue"), criteria.getStoreType()));
+    }
+
     List<Store> storeList =
         this.getEntityManager().createQuery(query.where(condition)).getResultList();
-    
+
     return storeList;
   }
 
--- TTO_MAIN/src/com/group/tto/main/service/StoreService.java
+++ TTO_MAIN/src/com/group/tto/main/service/StoreService.java
@@ -1,10 +1,15 @@
 package com.group.tto.main.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.group.tto.cmn.model.Store;
+import com.group.tto.main.common.StoreSearchCriteria;
 
 
 public interface StoreService {
-	public Store getStoreById(int sid);
+  public Store getStoreById(int sid);
+
+  public List<Store> getStoresbyCriteria(StoreSearchCriteria criteria);
 }
