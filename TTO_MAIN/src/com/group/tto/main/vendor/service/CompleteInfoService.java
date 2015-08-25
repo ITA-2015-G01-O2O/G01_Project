d9fbdcbc09d6958521886a6e33b9ff3718d53847
zhensu <suky.zhen@oocl.com>
2015-08-25 10:41:47 +0800

    update service of vendor

--- TTO_MAIN/src/com/group/tto/main/vendor/service/CompleteInfoService.java
+++ TTO_MAIN/src/com/group/tto/main/vendor/service/CompleteInfoService.java
@@ -3,5 +3,5 @@ package com.group.tto.main.vendor.service;
 import com.group.tto.cmn.model.Store;
 
 public interface CompleteInfoService {
-  public void updateStoreInfo(Store store);
+  public int updateStoreInfo(Store store,int sid);
 }
