8333a29e924c56284709cd781f7b1306bc7a2ce7
wangcd <cd.wang@oocl.com>
2015-08-25 17:55:09 +0800

    add file support

---
+++ TTO_MAIN/src/com/group/tto/main/img/ImgHelper.java
@@ -0,0 +1,9 @@
+package com.group.tto.main.img;
+
+import java.io.InputStream;
+
+public class ImgHelper {
+  public void getImg(InputStream imgInputSteam,String uuid){
+    
+  }
+}
---
+++ TTO_MAIN/src/com/group/tto/main/service/FileService.java
@@ -0,0 +1,6 @@
+package com.group.tto.main.service;
+
+
+public interface FileService {
+  public byte[] getBy(String uuid);
+}
