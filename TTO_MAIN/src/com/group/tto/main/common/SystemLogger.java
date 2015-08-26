1734f117cee8384123a1bdeeca0157094fda8137
wangcd <cd.wang@oocl.com>
2015-08-26 22:50:38 +0800

    COMMON - add logger

---
+++ TTO_MAIN/src/com/group/tto/main/common/SystemLogger.java
@@ -0,0 +1,17 @@
+package com.group.tto.main.common;
+
+import org.slf4j.LoggerFactory;
+
+public class SystemLogger {
+  public void info(LoggerNames loggerNames, String info) {
+    LoggerFactory.getLogger(loggerNames.toString()).info(info);
+  }
+
+  public void debug(LoggerNames loggerNames, String info) {
+    LoggerFactory.getLogger(loggerNames.toString()).debug(info);
+  }
+
+  public void error(LoggerNames loggerNames, String info) {
+    LoggerFactory.getLogger(loggerNames.toString()).error(info);
+  }
+}
