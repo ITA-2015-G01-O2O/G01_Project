598bee047617b579089b77e3529baf121425b781
SHENJO <JOEY.SHEN@OOCL.COm>
2015-08-27 22:59:00 +0800

    update user center

--- TTO_MAIN/src/com/group/tto/main/dao/CollectDao.java
+++ TTO_MAIN/src/com/group/tto/main/dao/CollectDao.java
@@ -6,17 +6,21 @@ import com.group.tto.cmn.model.Collect;
 import com.group.tto.cmn.model.Store;
 
 public interface CollectDao {
-	
-	public List<Collect> findAllCollects();
-	
-	public Long findAllCollectsByStore(Store store);
-	
-	public void removeCollectNodeById(Long id);
-	
-	public Collect findCollectNodeById(Long id);
-	
-    public void addCollect(Collect c);
-    
-    public List<Collect> findStoresByUserProfileId(Long userProfileId);
-    
+
+  public List<Collect> findAllCollects();
+
+  public Long findAllCollectsByStore(Store store);
+
+  public void removeCollectNodeById(Long id);
+
+  public Collect findCollectNodeById(Long id);
+
+  public void addCollect(Collect c);
+
+  public List<Collect> findStoresByUserProfileId(Long userProfileId);
+
+  public Collect findBy(Long storeId, Long userId);
+
+  public void save(Collect collect);
+
 }
